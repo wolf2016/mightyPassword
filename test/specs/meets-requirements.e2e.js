aae53e23ab069dfc939c1a74b53fc84761ad10c4
Wolf <wolf.v2021@gmail.com>
2021-03-28 02:04:42 -0400

    Iteration 2 complete
    I kept with my assumption that special characters are valid when the other requirements are met in this iteration. Because of that, entering 8 or more special characters without any numbers or letters will falsely trigger the not min length error, but adding one of each will pass it. I will move forward with this being a known issue.

--- test/specs/meets-requirements.e2e.js
+++ test/specs/meets-requirements.e2e.js
@@ -23,7 +23,7 @@ describe('Password Strength Check', () => {
     expect(PasswordPage.feedbackContainer).toHaveElementClass('strong-pw');
     expect(PasswordPage.feedbackContainer).not.toHaveElementClass('weak-pw');
   });
-  it('should show the password is weak', () => {
+  it('should show the password is weak and why', () => {
     PasswordPage.open();
 
     PasswordPage.checkPassword('12345');
@@ -40,7 +40,53 @@ describe('Password Strength Check', () => {
     expect(PasswordPage.passwordInput).toHaveValue('12345');
 
     //verify the feedback is correctly displayed
-    expect(PasswordPage.feedback).toHaveText('The password entered is not acceptable!');
+    expect(PasswordPage.feedback).toHaveText('The password entered is not acceptable' +
+      ' because it does not contain any letters and it is not the minimum length');
+    expect(PasswordPage.feedbackContainer).not.toHaveElementClass('strong-pw');
+    expect(PasswordPage.feedbackContainer).toHaveElementClass('weak-pw');
+  });
+  it('should show the password is weak and why', () => {
+    PasswordPage.open();
+
+    PasswordPage.checkPassword('&');
+    //verify objects that should be static on the page haven't changed
+    expect(browser).toHaveTitle('Mighty Strong Password Checker')
+    expect(PasswordPage.passwordLabel).toBeVisible();
+    expect(PasswordPage.passwordLabel).toHaveText('Enter Password:');
+    expect(PasswordPage.submitBtn).toBeVisible();
+    expect(PasswordPage.submitBtn).toHaveValue('Check Password');
+    expect(PasswordPage.submitBtn).toBeClickable();
+
+    //verify the value is still what was entered.
+    expect(PasswordPage.passwordInput).toBeVisible();
+    expect(PasswordPage.passwordInput).toHaveValue('&');
+
+    //verify the feedback is correctly displayed
+    expect(PasswordPage.feedback).toHaveText('The password entered is not acceptable' +
+      ' because it does not contain any numbers, it does not contain any letters,' +
+      ' and it is not the minimum length');
+    expect(PasswordPage.feedbackContainer).not.toHaveElementClass('strong-pw');
+    expect(PasswordPage.feedbackContainer).toHaveElementClass('weak-pw');
+  });
+  it('should show the password is weak and why', () => {
+    PasswordPage.open();
+
+    PasswordPage.checkPassword('abcdefgh');
+    //verify objects that should be static on the page haven't changed
+    expect(browser).toHaveTitle('Mighty Strong Password Checker')
+    expect(PasswordPage.passwordLabel).toBeVisible();
+    expect(PasswordPage.passwordLabel).toHaveText('Enter Password:');
+    expect(PasswordPage.submitBtn).toBeVisible();
+    expect(PasswordPage.submitBtn).toHaveValue('Check Password');
+    expect(PasswordPage.submitBtn).toBeClickable();
+
+    //verify the value is still what was entered.
+    expect(PasswordPage.passwordInput).toBeVisible();
+    expect(PasswordPage.passwordInput).toHaveValue('abcdefgh');
+
+    //verify the feedback is correctly displayed
+    expect(PasswordPage.feedback).toHaveText('The password entered is not acceptable' +
+      ' because it does not contain any numbers');
     expect(PasswordPage.feedbackContainer).not.toHaveElementClass('strong-pw');
     expect(PasswordPage.feedbackContainer).toHaveElementClass('weak-pw');
   });
