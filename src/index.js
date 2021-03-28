fee7d4ac5ab5ab28db7ed34887befd65c5b67415
Wolf <wolf.v2021@gmail.com>
2021-03-28 03:30:24 -0400

    iteration3 complete
    I also fixed my counter in the whyWeak function I created. In regards to my failure to see the obvious when it comes to the min length, I have no words. I am laughing and trying to get over it.  I was too focused on regex when there were simpler options.

--- src/index.js
+++ src/index.js
@@ -1,35 +1,62 @@
+//checks the password and sets the feedback
 checkPassword = () => {
-  const requirements = new RegExp(/^(?=.*\d)(?=.*[a-zA-Z]).{8,}$/);
   const feedbackP = document.getElementById("feedback");
   const feedbackDiv = document.getElementById("fDiv");
   let pw = document.getElementById("pw");
-  if (requirements.test(pw.value)) {
+  const isAdmin = document.getElementById("isAdmin").checked;
+  if (pickReqs(isAdmin).test(pw.value)) {
     feedbackDiv.classList.add("strong-pw");
     feedbackDiv.classList.remove("weak-pw");
     feedbackP.innerHTML = "The password entered is acceptable!";
   } else {
     feedbackDiv.classList.add("weak-pw");
     feedbackDiv.classList.remove("strong-pw");
-    feedbackP.innerHTML = "The password entered is not acceptable because" + whyWeak(pw);
+    feedbackP.innerHTML = "The password entered is not acceptable because " + whyWeak(pw, isAdmin);
   }
 }
 
-whyWeak = (pw) => {
+//tells the user why the password is weak
+whyWeak = (pw, isAdmin) => {
+  //the array and counter are for minimal formatting when giving results to user
   let reason = [];
   let i = 0;
+  //check for at least 1 number
   if (!new RegExp(/^(?=.*\d)/).test(pw.value)) {
-    reason.push(" it does not contain any numbers");
+    reason.push("it does not contain any numbers");
     i++;
   }
+  //check for at least one letter
   if (!new RegExp(/^(?=.*[a-zA-Z])/).test(pw.value)) {
-    reason.push(" it does not contain any letters");
+    reason.push("it does not contain any letters");
+    i++;
   }
-  if (!new RegExp(/^[A-Za-z0-9]{8,}$/).test(pw.value)) {
-    reason.push(" and it is not the minimum length");
+  if (!new RegExp(/^(?=.*[!@#$%^&*])/).test(pw.value) && isAdmin) {
+    reason.push("it does not contain any special characters");
+    i++;
+  }
+  //check alphanumeric length. Will fail with only special characters
+  if (pw.value.length < 8 && !isAdmin) {
+    reason.push("it is not the minimum length");
+    i++;
+  }
+  if (pw.value.length < 13 && isAdmin) {
+    reason.push("it is not the minimum length");
+    i++;
   }
   if (reason.length > 2) {
-    return reason.join(',');
+    return reason.join(', ');
+  } else if (reason.length === 2) {
+    return reason.join(' and ');
   } else {
     return reason.join(' ');
+  }
+}
+
+//figures out which requirements to use when checking the password
+pickReqs = (isAdmin) => {
+  if (!isAdmin) {
+    return new RegExp(/^(?=.*\d)(?=.*[a-zA-Z]).{8,}$/);;
+  } else {
+    return new RegExp(/^(?=.*\d)(?=.*[a-zA-Z])(?=.*[!@#$%^&*]).{13,}$/);
   }
 }
