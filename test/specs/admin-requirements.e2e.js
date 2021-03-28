bc948c4627c8fd7ca73bc04ff5a3c2150adf5f17
Wolf <wolf.v2021@gmail.com>
2021-03-28 05:08:38 -0400

    iteration 4 complete
    finished out with the task. May take a few hours to check that there aren't small bugs or things I missed.

--- test/specs/admin-requirements.e2e.js
+++ test/specs/admin-requirements.e2e.js
@@ -1,9 +1,9 @@
 const PasswordPage = require('./../pageobjects/password.page');
 
 describe('Password Strength Check for admin users', () => {
-  it('should say password1234! is strong', () => {
+  it('should say password1234!@# is strong', () => {
     PasswordPage.open();
-    PasswordPage.checkAdminPassword('password1234!');
+    PasswordPage.checkAdminPassword('password1234!@#');
 
     //verify objects that should be static on the page haven't changed
     expect(browser).toHaveTitle('Mighty Strong Password Checker')
@@ -17,7 +17,7 @@ describe('Password Strength Check for admin users', () => {
 
     //verify the value is still what was entered.
     expect(PasswordPage.passwordInput).toBeVisible();
-    expect(PasswordPage.passwordInput).toHaveValue('password1234!');
+    expect(PasswordPage.passwordInput).toHaveValue('password1234!@#');
     expect(PasswordPage.adminCkbox).toBeVisible();
     expect(PasswordPage.adminCkbox).toBeChecked();
 
@@ -48,14 +48,14 @@ describe('Password Strength Check for admin users', () => {
 
     //verify the feedback is correctly displayed
     expect(PasswordPage.feedback).toHaveText('The password entered is not acceptable' +
-      ' because it does not contain any special characters and it is not the minimum length');
+      ' because it does not contain at least 3 special characters and it is not the minimum length');
     expect(PasswordPage.feedbackContainer).not.toHaveElementClass('strong-pw');
     expect(PasswordPage.feedbackContainer).toHaveElementClass('weak-pw');
   });
-  it('should show 123456789101112 is weak and why', () => {
+  it('should show password1234! is weak and why', () => {
     PasswordPage.open();
 
-    PasswordPage.checkAdminPassword('123456789101112');
+    PasswordPage.checkAdminPassword('password1234!');
     //verify objects that should be static on the page haven't changed
     expect(browser).toHaveTitle('Mighty Strong Password Checker')
     expect(PasswordPage.passwordLabel).toBeVisible();
@@ -68,13 +68,13 @@ describe('Password Strength Check for admin users', () => {
 
     //verify the value is still what was entered.
     expect(PasswordPage.passwordInput).toBeVisible();
-    expect(PasswordPage.passwordInput).toHaveValue('123456789101112');
+    expect(PasswordPage.passwordInput).toHaveValue('password1234!');
     expect(PasswordPage.adminCkbox).toBeVisible();
     expect(PasswordPage.adminCkbox).toBeChecked();
 
     //verify the feedback is correctly displayed
     expect(PasswordPage.feedback).toHaveText('The password entered is not acceptable' +
-      ' because it does not contain any letters and it does not contain any special characters');
+      ' because it does not contain at least 3 special characters');
     expect(PasswordPage.feedbackContainer).not.toHaveElementClass('strong-pw');
     expect(PasswordPage.feedbackContainer).toHaveElementClass('weak-pw');
   });
@@ -101,7 +101,7 @@ describe('Password Strength Check for admin users', () => {
     //verify the feedback is correctly displayed
     expect(PasswordPage.feedback).toHaveText('The password entered is not acceptable' +
       ' because it does not contain any numbers, it does not contain any letters,' +
-      ' it is not the minimum length');
+      ' it does not contain at least 3 special characters, it is not the minimum length');
     expect(PasswordPage.feedbackContainer).not.toHaveElementClass('strong-pw');
     expect(PasswordPage.feedbackContainer).toHaveElementClass('weak-pw');
   });
@@ -127,7 +127,33 @@ describe('Password Strength Check for admin users', () => {
 
     //verify the feedback is correctly displayed
     expect(PasswordPage.feedback).toHaveText('The password entered is not acceptable' +
-      ' because it does not contain any numbers and it does not contain any special characters');
+      ' because it does not contain any numbers and it does not contain at least 3 special characters');
+    expect(PasswordPage.feedbackContainer).not.toHaveElementClass('strong-pw');
+    expect(PasswordPage.feedbackContainer).toHaveElementClass('weak-pw');
+  });
+  it('should show &^@#&^!@&#^@!# is weak and why', () => {
+    PasswordPage.open();
+
+    PasswordPage.checkAdminPassword('&^@#&^!@&#^@!#');
+    //verify objects that should be static on the page haven't changed
+    expect(browser).toHaveTitle('Mighty Strong Password Checker')
+    expect(PasswordPage.passwordLabel).toBeVisible();
+    expect(PasswordPage.passwordLabel).toHaveText('Enter Password:');
+    expect(PasswordPage.submitBtn).toBeVisible();
+    expect(PasswordPage.submitBtn).toHaveValue('Check Password');
+    expect(PasswordPage.submitBtn).toBeClickable();
+    expect(PasswordPage.adminLabel).toBeVisible();
+    expect(PasswordPage.adminLabel).toHaveText('Check the box if you are an admin');
+
+    //verify the value is still what was entered.
+    expect(PasswordPage.passwordInput).toBeVisible();
+    expect(PasswordPage.passwordInput).toHaveValue('&^@#&^!@&#^@!#');
+    expect(PasswordPage.adminCkbox).toBeVisible();
+    expect(PasswordPage.adminCkbox).toBeChecked();
+
+    //verify the feedback is correctly displayed
+    expect(PasswordPage.feedback).toHaveText('The password entered is not acceptable' +
+      ' because it does not contain any numbers and it does not contain any letters');
     expect(PasswordPage.feedbackContainer).not.toHaveElementClass('strong-pw');
     expect(PasswordPage.feedbackContainer).toHaveElementClass('weak-pw');
   });
